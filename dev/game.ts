80a969a97b018b12aa64855cec4f1937222c4fb2
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-06-04 13:29:52 +0200

    Made one Car when yo click it will honk

--- dev/game.ts
+++ dev/game.ts
@@ -1,9 +1,11 @@
-
+/// <reference path="car.ts"/>
 
 class Game {
      
     constructor() {
+        let car : Car = new Car();
     }
+    
     
     private gameLoop(){
     }
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,33 @@
+var Car = (function () {
+    function Car() {
+        this.color = Math.random() * 360;
+        this.div = document.createElement("car");
+        document.body.appendChild(this.div);
+        this.div.addEventListener("click", function () {
+            var audio = new Audio('../docs/audio/Car_horn.mp3');
+            audio.play();
+            console.log("Vroem Vroem");
+        });
+        var x = 1 * window.innerWidth;
+        var y = 0.70 * window.innerHeight;
+        this.move(x, y);
+        this.div.style.webkitFilter = "hue-rotate(" + this.color + "deg)";
+        this.div.style.filter = "hue-rotate(" + this.color + "deg)";
+    }
+    Car.prototype.move = function (x, y) {
+        this.x = x;
+        this.y = y;
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+    };
+    Car.prototype.speak = function () {
+        console.log("Vroem Vroem");
+    };
+    return Car;
+}());
 var Game = (function () {
     function Game() {
+        var car = new Car();
     }
     Game.prototype.gameLoop = function () {
     };
@@ -8,3 +36,4 @@ var Game = (function () {
 window.addEventListener("load", function () {
     new Game();
 });
+//# sourceMappingURL=main.js.map
