7968ac9c56b167644b8697fbfa9c0f7ec3517272
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-06-06 16:09:16 +0200

    Fixed bug with road.
    Now the moving car's are a inheritnace from the Vehicle.ts
    
    Small performance boost.

--- dev/car.ts
+++ dev/car.ts
@@ -1,35 +1,5 @@
-class Car {
-    //Color
-    color: number;
-
-    constructor(a: number, b: number, div: HTMLElement, g: number) {
-        this.color = Math.random() * 360;
-
-        document.body.appendChild(div);
-        div.addEventListener("click", function () {
-            let audio = new Audio('../docs/audio/Car_horn.mp3');
-            audio.play();
-        });
-
-        var x = a * window.innerWidth;
-        var y = b * window.innerHeight;
-
-        this.move(x, y, div);
-        this.fuel(g);
-
-        div.style.webkitFilter = "hue-rotate(" + this.color + "deg)";
-        div.style.filter = "hue-rotate(" + this.color + "deg)";
-    }
-
-    move(x: number, y: number, div: HTMLElement) {
-        x = x;
-        y = y;
-
-        div.style.left = x + "px";
-        div.style.top = y + "px";
-    }
-
-    fuel(g: number) {
-     console.log('Je heb top dit moment nog:',g,'liter bezine');
-    }
+class Car extends Vehicle {
+constructor(a: number, b: number, div: HTMLElement, g: number){
+ super(a,b,div,g);
+}
 }
--- dev/game.ts
+++ dev/game.ts
@@ -17,15 +17,15 @@ class Game {
 
     private Gameloop(i: number){
         i = i/10;
-        this.road.push(new Road(0.5,i,document.createElement("road")));
         this.road.push(new Road(i,0.5,document.createElement("road_turn")));
+        this.road.push(new Road(0.5,i,document.createElement("road")));
     } 
 
     private Spawn(){
-        let car = new Car(0.55,1,document.createElement("car"),100); 
-        let car_return = new Car(0.51,0,document.createElement("car_return"),100);
-        let car_right = new Car(1,0.57,document.createElement("car_right"),100);
-        let car_left = new Car(0,0.51,document.createElement("car_left"),100);
+        let car = new Vehicle(0.55,1,document.createElement("car"),100); 
+        let car_return = new Vehicle(0.51,0,document.createElement("car_return"),100);
+        let car_right = new Vehicle(1,0.57,document.createElement("car_right"),100);
+        let car_left = new Vehicle(0,0.51,document.createElement("car_left"),100);
     }  
 } 
 
