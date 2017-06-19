be89b30eeeab185196105d41eb4c6ab52d729b7e
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-06-19 15:42:48 +0200

    Test for mobile

--- docs/js/main.js
+++ docs/js/main.js
@@ -133,10 +133,32 @@ var Game = (function () {
     };
     return Game;
 }());
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 var start = document.createElement("button");
 document.body.appendChild(start);
 start.innerHTML = "Klik om het spel te starten!";
 start.addEventListener("click", function () {
+    if (isMobile.any())
+        alert('Mobile');
     alert("Welkom bij Jeroen City, Veel speel plezier!");
     start.remove();
     new Game();
