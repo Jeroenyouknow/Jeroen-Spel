5d0de2f9249419c9beee4c4d5744f3b8b5f11c12
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-06-23 14:45:43 +0200

    Hot fix

--- dev/recreation_district.ts
+++ dev/recreation_district.ts
@@ -1,6 +1,6 @@
 class Recreation extends District {
     constructor() {
-        super(0.75, 0.65, 0.582, 0.595, document.createElement("buy_1"), document.createElement("recreation"), 500, 800,'Met het Recreation District krijg je om de 5 secoden $100 inkomsten') ;
+        super(0.75, 0.65, 0.582, 0.595, document.createElement("buy_1"), document.createElement("recreation"), 500, 800, 'Met het Recreation District krijg je om de 5 secoden $100 inkomsten');
         setInterval(() => this.addMoney(), 5000);
 
         this.district.addEventListener("click", function () {
