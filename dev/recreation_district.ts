502942877ea15842c9901515f1d4dbffaa49eb1a
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-06-24 18:28:05 +0200

    Value of district to 500

--- dev/recreation_district.ts
+++ dev/recreation_district.ts
@@ -1,7 +1,7 @@
 class Recreation extends District {
     constructor() {
         super(0.75, 0.65, 0.582, 0.595, document.createElement("buy_1"), document.createElement("recreation"), 500, 800, 'Met het Recreation District krijg je om de 5 secoden $500 inkomsten');
-        setInterval(() => this.addMoney(), 5000);
+        setInterval(() => this.addMoney(), 500);
 
         this.district.addEventListener("click", function () {
             let audio = new Audio('audio/recreation.mp3');
