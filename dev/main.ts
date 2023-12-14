b2ae1631b2d093ab1956e03f0a85a506c6fb8539
Jeroen H <jeroen-h@hotmail.nl>
2023-12-14 20:23:00 +0100

    Hope this works....

--- dev/main.ts
+++ dev/main.ts
@@ -27,21 +27,6 @@ let start = document.createElement("button");
 document.body.appendChild(start);
 start.innerHTML = "Klik om het spel te starten!";
 start.addEventListener("click", function () {
-    // if (isMobile.any()) {
-    //     alert('Het spijt me maar Jeroen City is op dit nog niet beschikbaar op mobile devices. Probeer het via een Laptop of desktop nog een keer! ');
-    // }
-
-    // else {
-    //     start.remove();
-    //      let game = new Game();
-    //     alert("Welkom bij RAC-Software eerst een kleine uitleg:");
-    //     alert("Je moet eerst een district kopen");
-    //     alert("Met een district verdien je geld");
-    //     alert("Als je meer dan $5000 hebt verdiend heb je het spel gehaald");
-    //     alert(" Veel Speelplezier :)");
-    //
-    //     audio.pause();
-    // }
     start.remove();
     let game = new Game();
 });
